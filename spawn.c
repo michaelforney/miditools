9f85b91ec318ce7b882b8fc252fa20e7a25de1ca
Michael Forney <mforney@mforney.org>
2025-02-26 00:53:49 -0800

    spawn: Handle case where the file descriptor does not need dup2
    dup2 won't clear FD_CLOEXEC in this case, so don't set it and close
    manually.

--- spawn.c
+++ spawn.c
@@ -13,23 +13,19 @@ spawn(const char *path, char *const argv[], int mode, int fd[2])
 	if (mode & READ) {
 		if (pipe(p) != 0)
 			fatal("pipe:");
-		if (fcntl(p[0], F_SETFD, FD_CLOEXEC) != 0)
-			fatal("fcntl FD_CLOEXEC:");
-		if (fcntl(p[1], F_SETFD, FD_CLOEXEC) != 0)
-			fatal("fcntl FD_CLOEXEC:");
 		t[1] = p[1];
 		p[1] = fd[0];
+		if (fcntl(t[1], F_SETFD, FD_CLOEXEC) != 0)
+			fatal("fcntl FD_CLOEXEC:");
 	}
 	if (mode & WRITE) {
 		if (pipe(p + 2) != 0)
 			fatal("pipe:");
-		if (fcntl(p[2], F_SETFD, FD_CLOEXEC) != 0)
-			fatal("fcntl FD_CLOEXEC:");
-		if (fcntl(p[3], F_SETFD, FD_CLOEXEC) != 0)
-			fatal("fcntl FD_CLOEXEC:");
 		t[0] = p[2];
 		p[2] = p[3];
 		p[3] = fd[1];
+		if (fcntl(t[0], F_SETFD, FD_CLOEXEC) != 0)
+			fatal("fcntl FD_CLOEXEC:");
 	}
 	if (mode == (READ | WRITE) && p[1] == p[2]) {
 		/* swap dup order to avoid clobber */
@@ -47,10 +43,16 @@ spawn(const char *path, char *const argv[], int mode, int fd[2])
 		fd[0] = t[0];
 		fd[1] = t[1];
 	} else {
-		if (p[0] != -1 && dup2(p[0], p[1]) < 0)
-			fatal("dup2:");
-		if (p[2] != -1 && dup2(p[2], p[3]) < 0)
-			fatal("dup2:");
+		if (p[0] != -1 && p[0] != p[1]) {
+			if (dup2(p[0], p[1]) < 0)
+				fatal("dup2:");
+			close(p[0]);
+		}
+		if (p[2] != -1 && p[2] != p[3]) {
+			if (dup2(p[2], p[3]) < 0)
+				fatal("dup2:");
+			close(p[2]);
+		}
 		execvp(argv[0], argv);
 		fatal("exec %s:", argv[0]);
 	}
